b94c864133a5d5f4d0ded0b6882eb555c3d25201
Michael Hallmann <halli@gmx.de>
2023-12-09 11:15:23 +0100

    new func sq

--- src/halli.py
+++ src/halli.py
@@ -1,2 +1,5 @@
 def func(x):
     return x + 1
+
+def squ(n: int) -> int:
+    return n * n
--- test/test_halli.py
+++ test/test_halli.py
@@ -3,3 +3,9 @@ import halli
 def test_answer():
     assert halli.func(4) == 5
     assert halli.func(3) == 4
+
+
+def test_sq():
+    assert halli.sq(2) == 4
+    assert halli.sq(3) == 9
+    
